670c7558e2bbde54020940fae79b9a7fd224b746
abecel22 <abe2211@gmail.com>
2019-03-13 23:32:40 -0500

    Updated git- testing

--- src/components/backdrop.js
+++ src/components/backdrop.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "./styles/backdrop.css"
 
-const backdrop = props => <div className="backdrop" onClick={props.click}/>
+const backdrop = props => <div className="backdrop" onClick={props.click} />
 
 export default backdrop
