831785108730a59b7a0337bacfec8005f5c9bc3f
abecel22 <abe2211@gmail.com>
2019-03-21 21:25:20 -0500

    Updated arousel-  buyers feedback jsx and css.

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -3,8 +3,9 @@ import Slider from "react-slick"
 
 class Carousel extends React.Component {
   render() {
-    var settings = {
+    let settings = {
       dots: true,
+      className: "slider-div",
       infinite: true,
       speed: 600,
       slidesToShow: 1,
@@ -14,28 +15,22 @@ class Carousel extends React.Component {
       pauseOnHover: false,
       fade: true,
     }
+
+    const reviews = [
+      " Fast shipper! Excellent customer service! Just as described! Thanks again!",
+      "Excellent seller, well packed and quick shipping, highly recommend!!!",
+      " High quality, works great. Thank you for good packaging and fast service!",
+      "Awesome product, fast shipping! A+++ seller, highly recommend!",
+      "Great Seller. Highly Recommended. Would Do Business Again. 5 stars*****",
+    ]
+
     return (
       <Slider {...settings}>
-        <div>
-          <h4>
-            Fast shipper! Excellent customer service! Just as described! Thanks
-            again!
-          </h4>
-        </div>
-        <div>
-          <h4>
-            {" "}
-            Excellent seller, well packed and quick shipping, highly
-            recommend!!!
-          </h4>
-        </div>
-        <div>
-          <h4>
-            {" "}
-            High quality, works great. Thank you for good packaging and fast
-            service!
-          </h4>
-        </div>
+        {reviews.map((review, index) => (
+          <div key={index}>
+            <p>{review}</p>
+          </div>
+        ))}
       </Slider>
     )
   }
