d308c43d0452fd32a938fcaa84b73c8e06409ada
abecel22 <abe2211@gmail.com>
2019-03-24 09:20:18 -0500

    Added favicon and gastby plugin

--- src/html.js
+++ src/html.js
@@ -11,6 +11,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+
         <link
           href="https://fonts.googleapis.com/css?family=Bitter"
           rel="stylesheet"
@@ -26,6 +27,7 @@ export default function HTML(props) {
           type="text/css"
           href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
         />
+
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
